c61e9b4e44df0e0fa7f9bd3112fc31e6974b0c23
Hector Palafox <hpalafoxp@outlook.com>
2026-01-13 17:35:41 -0800

    Created mean functions and tests for mean

--- src/dumbpy/arithmetic_mean.py
+++ src/dumbpy/arithmetic_mean.py
@@ -3,15 +3,15 @@ A module that calculates the arithmetic mean of a list of numbers
 
 """
 
-from support_functions import *
+from dumbpy.support_functions import *
 
-def arithmetic_mean(values: list[float | int | bool]) -> float | int:
+def arithmetic_mean(values: list[float | int | bool]) -> float:
     """
     Calculate the arithmetic mean of the inputted list and return the result.
 
     Parameters
     ----------
-    values : list[float | int]
+    values : list[float | int | bool]
         A list of numbers.
 
     Returns
@@ -24,4 +24,25 @@ def arithmetic_mean(values: list[float | int | bool]) -> float | int:
     >>> arithmetic_mean([1, 2, 3, 4])
     2.5
     """
-    pass
+    
+    ## This will raise errors if the value type is not compatible.
+    numbers: list[int | float | bool] = validate_list(values)
+    
+
+    n_elements: int = len(numbers)
+
+    if not (n_elements > 0):
+        raise ValueError(
+            "The number list needs to have at least one numeric element"
+        )
+
+    addition: int | float = 0
+
+    for x in numbers:
+        addition += x
+    
+    return addition / n_elements
+    
+
+
+
